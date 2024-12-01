c07a99be9c2c8b64fc87b48fc0ec0dc05a12c6ee
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-02 01:50:23 +0200

    create app bar

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -5,6 +5,28 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "News",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Cloud",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.orange,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

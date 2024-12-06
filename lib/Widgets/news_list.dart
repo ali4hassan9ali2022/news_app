f3608e2cd58ebdfc1351ea9bc7e6cb6145cb9054
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-06 10:52:39 +0200

    finish news list

--- lib/Widgets/news_list.dart
+++ lib/Widgets/news_list.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/Models/articles_model.dart';
 import 'package:news_app/Widgets/news_tile.dart';
 
 class NewsList extends StatelessWidget {
-  const NewsList({super.key});
-
+  const NewsList({super.key, required this.articles});
+  final List<ArticlesModel> articles;
   @override
   Widget build(BuildContext context) {
     return SliverList(
         delegate: SliverChildBuilderDelegate(
-      childCount: 10,
+      childCount: articles.length,
       (context, index) {
-        return const Padding(
-          padding: EdgeInsets.only(bottom: 20),
-          child: NewsTile(),
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: NewsTile(
+            articlesModel: articles[index],
+          ),
         );
       },
     ));
